6758ec6576c6ae41f9b5755d7ee6a36e47679696
Fernando Perulan <82981866+FernandoPerulan@users.noreply.github.com>
2025-08-06 00:18:48 -0300

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ print("Hello, World!")
 
 import pandas as pd
 
-df = pd.read_excel("data/ventas.xlsx")
+df = pd.read_excel("Data/ventas.xlsx")
 print("Filas:", len(df))
 df.to_csv("resultados.csv", index=False)
